cd01da4f3e30f5deec30351263aa3582c1666e8c
Sachin Gupta <sachin.gupta@peason.com>
2025-07-25 23:21:34 +0530

    Test | Describe & Its Grouping

--- src/Greet/greet.test.tsx
+++ src/Greet/greet.test.tsx
@@ -1,14 +1,17 @@
-import { render ,screen} from "@testing-library/react";
-import Greet from './index'
+import { render, screen } from "@testing-library/react";
+import Greet from "./index";
 
-test("Greet render successfully", () => {
-  render(<Greet />);
-  const element = screen.getByText('Hello');
-  expect(element).toBeInTheDocument();
-});
+describe("Greet Test:::::::", () => {
+  test("render successfully", () => {
+    render(<Greet />);
+    const element = screen.getByText("Hello");
+    expect(element).toBeInTheDocument();
+  });
 
-test('Greet render with name',()=>{
-    render(<Greet name="sachin"/>);
-    const element = screen.getByText("Hello sachin")
-    expect(element).toBeInTheDocument()
+  
+  test("render with name", () => {
+    render(<Greet name="sachin" />);
+    const element = screen.getByText("Hello sachin");
+    expect(element).toBeInTheDocument();
+  });
 });
