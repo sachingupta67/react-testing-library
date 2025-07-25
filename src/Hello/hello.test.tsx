15526ab60f0447d1a0b8eae932719c3a1559755c
Sachin Gupta <sachin.gupta@peason.com>
2025-07-25 22:44:27 +0530

    Hello Test

--- src/Hello/hello.test.tsx
+++ src/Hello/hello.test.tsx
@@ -1,9 +1,8 @@
-import { render, screen } from '@testing-library/react'
-import { Hello } from './index'
+import { render, screen } from "@testing-library/react";
+import { Hello } from "./index";
 
-describe('Hello component', () => {
-  it('renders with correct text', () => {
-    render(<Hello name="Sachin" />)
-    expect(screen.getByText('Hello, Sachin!')).toBeInTheDocument()
-  })
-})
+test("Hello component", () => {
+  render(<Hello name="Sachin" />); // Virtual DOM of Hello Component
+  const element= screen.getByText(/Hello, sachin!/i)// Screen has Object which has query to Virtual DOM , we used can insensitive regex
+  expect(element).toBeInTheDocument(); 
+});
